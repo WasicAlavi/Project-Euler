4b4010c6f4b9532ea77d003cfc2e6e49263ac59e
Wasic Alavi Sadmam <141858968+WasicAlavi@users.noreply.github.com>
2023-08-14 02:20:02 +0600

    Update Project Euler-02.js

---
+++ Project Euler-02.js
@@ -0,0 +1,11 @@
+let arr=[1,2];
+let sum=0;
+for(i=3;i<35;i++){
+  arr.push(arr[i-3]+arr[i-2]);
+  let a=arr.length-3;
+  let b= arr[a];
+  if(b<=4000000 && b%2==0){
+   sum+=b;
+} 
+}
+console.log(sum)
